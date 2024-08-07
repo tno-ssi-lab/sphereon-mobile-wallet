f7c493b1dbeecfdc908e7fc79f604f89fcee27ee
Odysseas Zafeiriou <odysseas@wearereasonablepeople.com>
2024-08-07 14:44:05 +0200

    Prettier fixes

--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -220,8 +220,8 @@ const CredentialsStack = (): JSX.Element => {
           header: (props: NativeStackHeaderProps) => (
             <SSIHeaderBar
               {...props}
-            // TODO rethink back button visibility for Android
-            //showBackButton={Platform.OS === PlatformsEnum.IOS}
+              // TODO rethink back button visibility for Android
+              //showBackButton={Platform.OS === PlatformsEnum.IOS}
             />
           ),
         }}
@@ -341,8 +341,8 @@ const QRStack = (): JSX.Element => {
           header: (props: NativeStackHeaderProps) => (
             <SSIHeaderBar
               {...props}
-            // TODO rethink back button visibility for Android
-            //showBackButton={Platform.OS === PlatformsEnum.IOS}
+              // TODO rethink back button visibility for Android
+              //showBackButton={Platform.OS === PlatformsEnum.IOS}
             />
           ),
         }}
@@ -372,8 +372,9 @@ const QRStack = (): JSX.Element => {
               {...props}
               // TODO rethink back button visibility for Android
               //showBackButton={Platform.OS === PlatformsEnum.IOS}
-              headerSubTitle={`${translate('credentials_required_subtitle', {verifierName: route.params.verifierName})} ${route.params.presentationDefinition.purpose && `\n\n${route.params.presentationDefinition.purpose}`
-                }`}
+              headerSubTitle={`${translate('credentials_required_subtitle', {verifierName: route.params.verifierName})} ${
+                route.params.presentationDefinition.purpose && `\n\n${route.params.presentationDefinition.purpose}`
+              }`}
             />
           ),
         })}
@@ -590,7 +591,7 @@ export const OID4VCIStack = (): JSX.Element => {
             <SSIHeaderBar
               {...props}
               onBack={route.params.onBack}
-            // headerSubTitle={translate('browser_open_subtitle')}
+              // headerSubTitle={translate('browser_open_subtitle')}
             />
           ),
         })}
@@ -651,8 +652,8 @@ export const OID4VCIStack = (): JSX.Element => {
           header: (props: NativeStackHeaderProps) => (
             <SSIHeaderBar
               {...props}
-            // TODO rethink back button visibility for Android
-            //showBackButton={Platform.OS === PlatformsEnum.IOS}
+              // TODO rethink back button visibility for Android
+              //showBackButton={Platform.OS === PlatformsEnum.IOS}
             />
           ),
         }}
@@ -752,8 +753,9 @@ export const SiopV2Stack = (): JSX.Element => {
               onBack={route.params.onBack}
               // TODO rethink back button visibility for Android
               //showBackButton={Platform.OS === PlatformsEnum.IOS}
-              headerSubTitle={`${translate('credentials_required_subtitle', {verifierName: route.params.verifierName})} ${route.params.presentationDefinition.purpose && `\n\n${route.params.presentationDefinition.purpose}`
-                }`}
+              headerSubTitle={`${translate('credentials_required_subtitle', {verifierName: route.params.verifierName})} ${
+                route.params.presentationDefinition.purpose && `\n\n${route.params.presentationDefinition.purpose}`
+              }`}
             />
           ),
         })}
@@ -766,8 +768,8 @@ export const SiopV2Stack = (): JSX.Element => {
           header: (props: NativeStackHeaderProps) => (
             <SSIHeaderBar
               {...props}
-            // TODO rethink back button visibility for Android
-            //showBackButton={Platform.OS === PlatformsEnum.IOS}
+              // TODO rethink back button visibility for Android
+              //showBackButton={Platform.OS === PlatformsEnum.IOS}
             />
           ),
         }}
